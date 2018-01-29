2b68b9d479117a6437bf7adb62945a21fa78accd
brmsAdmin <>
2018-01-29 16:10:46 -0600

    {/smartmetervent/src/main/java/redhat/smartmetervent/EmailNotificationService.java}

--- smartmetervent/src/main/java/redhat/smartmetervent/EmailNotificationService.java
+++ smartmetervent/src/main/java/redhat/smartmetervent/EmailNotificationService.java
@@ -15,7 +15,10 @@ public class EmailNotificationService implements java.io.Serializable {
     
         java.util.List msg = new java.util.ArrayList();
         java.lang.String body = notification.getMessage();
-        
+      
+		String v1 = val.replace("#1", notification.getId);
+		String v2 = v1.replace("#2", notification.getOutageEvents().size());
+		
         msg.add(0,notification.getEmailId());
         msg.add(1,notification.getMessage());
         msg.add(2,"Electricity outage notification for " + notification.getId() );
